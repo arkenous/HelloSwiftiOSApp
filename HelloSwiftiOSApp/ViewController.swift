077480de7c069c498044032ac8fbe17a5c320900
Kensuke Kosaka <k@trileg.net>
2016-05-26 18:15:56 +0900

    ボタンを押すたびにでラベルの文字が変わるようにした

--- HelloSwiftiOSApp/ViewController.swift
+++ HelloSwiftiOSApp/ViewController.swift
@@ -9,17 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  @IBOutlet weak var sample_label: UILabel!
+  @IBOutlet weak var sample_button: UIButton!
+  
+  // 変数の宣言
+  var flg = false
+  
+  // 定数の宣言
+  let hello = "Hello"
+  let world = "World"
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    sample_label.text = "Hello World!"
+  }
+  
+  @IBAction func buttonTapped(sender: AnyObject) {
+    if flg == false {
+      sample_label.text = hello
+      flg = true
+    } else {
+      sample_label.text = world
+      flg = false
+    }
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
 }
 
